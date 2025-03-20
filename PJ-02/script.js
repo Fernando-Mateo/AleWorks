138ed30e1e588183c661cc0bb11cc988cb3b98c6
Fernando-Mateo <fernando.kehrwald@escola.pr.gov.br>
2025-03-20 08:17:45 -0300

    First commit

---
+++ PJ-02/script.js
@@ -0,0 +1,6 @@
+function scrollToGallery() {
+    const gallery = document.getElementById("gallery");
+    gallery.scrollIntoView({ behavior: "smooth" });
+}
+
+console.log("Explore a cultura do Sakura com nosso site!");
